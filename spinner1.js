59c4cefcbd1bdc7f34301c8fad1aa4280939f7c7
Elena Chernikova <lena@acvos.com>
2022-02-15 14:06:02 -0800

    eslint done

--- spinner1.js
+++ spinner1.js
@@ -1,40 +1,40 @@
-
-function spinner() {
+const spinner = function() {
   setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
+    process.stdout.write('\r|   ');
+  }, 100);
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, 300);
 
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, 500);
 
-setTimeout(() => {
+  setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   ');
-}, 700);
+    process.stdout.write('\r\\   ');
+  }, 700);
 
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, 900);
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, 1100);
 
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1300);
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, 1300);
 
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 1500);
+  setTimeout(() => {
+    process.stdout.write('\r\\   ');
+  }, 1500);
 
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1700);
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, 1700);
 };
+
 spinner();
